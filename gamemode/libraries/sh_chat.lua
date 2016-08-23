40b769a2c2fc4cba38b4f5e272d87ae89ea7eb4d
Brian Hang <hangwithbrian@gmail.com>
2016-08-23 14:01:58 -0700

    Started on nut.chat library

---
+++ gamemode/libraries/sh_chat.lua
@@ -0,0 +1,126 @@
+--[[
+File:    libraries/sh_chat.lua
+Purpose: Sets up a system to divide chat messages into different types.
+--]]
+
+nut.chat = {}
+nut.chat.modes = {}
+
+-- The first character after a chat mode prefix.
+local MSG_START = 2
+
+-- A function that always returns true.
+local ALWAYS_TRUE = function() return true end
+
+-- Gets a list of players that can receive a certain chat.
+function nut.chat.getRecipients(speaker, mode, message, context)
+    assert(type(speaker) == "Player", "speaker is not a player")
+    assert(IsValid(speaker), "speaker is not a valid player")
+
+    -- Default context to an empty table.
+    context = context or {}
+
+    -- Get information about the chat mode.
+    local info = nut.chat.modes[mode]
+
+    -- A list of players that can hear the speaker.
+    local recipients = {speaker}
+
+    if (not info) then
+        return recipients
+    end
+
+    -- Find which players can hear the speaker.
+    for _, listener in ipairs(player.GetAll()) do
+        if (mode.onCanHear(speaker, listener, message, context) or
+            hook.Run("IsChatRecipient", speaker, listener, mode,
+            message, context)) then
+            recipients[#recipients + 1] = listener
+        end
+    end
+
+    return recipients
+end
+
+-- Determines the type of chat message being sent from a given message.
+function nut.chat.parse(speaker, message)
+    assert(type(speaker) == "Player", "speaker is not a player")
+    assert(IsValid(speaker), "speaker is not a valid player")
+    assert(type(message) == "string")
+
+    -- A table to store information about the chat message.
+    local mode
+    local context = {}
+
+    -- Find the chat mode that matches.
+    for thisMode, info in pairs(nut.chat.modes) do
+        local prefix = info.prefix
+        local offset = info.noSpaceAfter and 0 or 1
+        local noSpaceAfter = info.noSpaceAfter
+
+        -- Check if the prefix for this chat mode matches the one in
+        -- the chat message.
+        if (type(prefix) == "table") then
+            -- If the prefix has aliases, check those as well.
+            for _, alias in pairs(prefix) do
+                if (message:sub(1, #alias + offset) ==
+                    alias..(noSpaceAfter and "" or " ")) then
+                    mode = thisMode
+                    prefix = alias
+
+                    break
+                end
+            end
+        elseif (type(prefix) == "string" and message:sub(1, #prefix + offset)
+                == prefix..(noSpaceAfter and "" or " ")) then
+                mode = thisMode
+
+                break
+        end
+
+        -- Change the message to only contain the real chat message.
+        if (mode == thisMode) then
+            message = message:sub(#prefix + 1)
+
+            -- Get rid of the space in front if needed.
+            if (not noSpaceAfter) then
+                message = message:sub(MSG_START)
+            end
+        end
+    end
+
+    -- Default the chat mode to unknown.
+    mode = mode or "unknown"
+    
+    -- Adjust the mode, context, and message.
+    mode = hook.Run("ChatAdjustMode", speaker, message, mode, context) or mode
+    hook.Run("ChatAdjustContext", speaker, mode, message, context)
+
+    return mode, messasge, context
+end
+
+-- Sets up a chat mode for use in the future.
+function nut.chat.register(mode, info)
+    mode = tostring(mode)
+    assert(type(info) == "table", "info for '"..mode.."' is not a table")
+
+    -- Convert the canHear function into one that returns whether or not
+    -- a player can be heard as a boolean.
+    if (type(info.onCanHear) == "number") then
+        local range = info.onCanHear
+
+        info.onCanHear = function(speaker, listener)
+            return speaker:GetPos():Distance(listener:GetPos()) <= range
+        end
+    else
+        info.onCanHear = ALWAYS_TRUE
+    end
+
+    -- Default the onCanSay to always return true.
+    info.onCanSay = info.onCanSay or ALWAYS_TRUE
+
+    if (
+end
+
+-- Sends a chat message using a given mode.
+function nut.chat.send(speaker, 
