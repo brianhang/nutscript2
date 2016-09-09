68c02ec5c1491c9a47165737da37bcd93d159b79
Brian Hang <hangwithbrian@gmail.com>
2016-09-08 18:25:57 -0700

    Added nut.gui to contain VGUI panels

--- gamemode/cl_init.lua
+++ gamemode/cl_init.lua
@@ -6,6 +6,9 @@ Purpose: Loads the client-side portion of NutScript.
 -- Set up the NutScript "namespace".
 nut = nut or {}
 
+-- Place to store VGUI panels.
+nut.gui = nut.gui or {}
+
 -- Include shared.lua to load all the framework files then setup.lua to set up
 -- the gamemode for use.
 include("shared.lua")
