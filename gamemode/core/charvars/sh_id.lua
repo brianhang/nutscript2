e29d097965deabbb7213e0e7fa39d9876e0a2b4a
Brian Hang <hangwithbrian@gmail.com>
2016-09-08 18:27:01 -0700

    Fixed ID being replicated when it should not

--- gamemode/core/charvars/sh_id.lua
+++ gamemode/core/charvars/sh_id.lua
@@ -4,9 +4,8 @@ Purpose: Adds a numeric identifier for a character.
 --]]
 
 nut.char.registerVar("id", {
-    default = -1,
     field = "id",
     isConstant = true,
     onSave = CHARVAR_NOSAVE,
-    replication = CHARVAR_PUBLIC,
+    replication = CHARVAR_NONE
 })
